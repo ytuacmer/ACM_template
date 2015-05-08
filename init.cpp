fe8030494b7a10fcafbd367ab06c380f9571f0c9
svtter <svtter@qq.com>
2015-05-08 11:22:45 +0800

    update init.cpp

--- init.cpp
+++ init.cpp
@@ -5,18 +5,22 @@ using namespace std;
 #define ll long long int
 #define MEM(a) memset(a, 0, sizeof(a))
 #define MEMM(a) memset(a, -1, sizeof(a))
-#define DEB(x, n) cout << (x) << " " << (n) << endl
-#define FOR(i, s, e) for(int (i) = (s); (i) < (e); (i)++)
+#define DEB(x, n) \
+	cout << (x) << " " << (n) << endl
+#define FOR(i, s, e) \
+	for(int (i) = (s); (i) < (e); (i)++)
 #define CR printf("\n")
+#define println(format, ...) ({\
+		printf(format "\n", __VA_ARGS__); })
 const double PI = acos(-1.0);
 
 int main()
 {
 #ifdef DEBUG
-    // freopen("input", "r", stdin);       //从input文件中读入
-    // freopen("output", "w", stdout);     //输出到output文件
+	// freopen("input", "r", stdin);       //从input文件中读入
+	// freopen("output", "w", stdout);     //输出到output文件
 #endif
 
-    return 0;
+	return 0;
 }
 
